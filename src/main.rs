abd800b1aef89c73be1c10186526bbfc3dc52f31
mikusugar <syfangjie@live.cn>
2023-04-03 15:23:41 +0800

    feat: 优化输出显示

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use glob::glob;
 use std::fs;
 use clap::Parser;
+use std::time::Instant;
 
 #[derive(Parser, Debug)]
 #[command(author = "mikusugar", version, about = "Helps delete Mac OS .DS_Stroe files", long_about = None)]
@@ -23,7 +24,8 @@ fn main() {
     if path.ends_with("/") {
         path.pop();
     }
-
+    let start_time = Instant::now();
+    let mut count = 0;
     let path = format!("{}/**/.DS_Store", path);
     println!("Search for {} ...", path);
     for entry in glob(&path).expect(&format!("Failed to read {}", path)) {
@@ -33,10 +35,15 @@ fn main() {
                     if args.show {
                         println!("rm file {:?}", path.display());
                     }
+                    count += 1;
                     fs::remove_file(path.as_path()).expect(&format!("Failed to delete file {}!", path.display()))
                 }
             }
             Err(e) => eprintln!("{:?}", e),
         }
     }
+
+    let end_time = Instant::now();
+    let time_elapsed = end_time.duration_since(start_time);
+    println!("{} files have been deleted,program execution time：{:?}", count, time_elapsed);
 }
